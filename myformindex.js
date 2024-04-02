fce9914918d124f9eb11d75740d95368a8501e22
meenapothappagari <157195252+meenapothappagari@users.noreply.github.com>
2024-04-02 17:43:21 +0530

    Add files via upload

---
+++ myformindex.js
@@ -0,0 +1,93 @@
+// let formtask=document.getElementById("form")
+// console.log(formtask);
+
+// let firstname=document.getElementById("firstname")
+// console.log(firstname.value);
+// document.addEventListener("input",function(){
+//     console.log(firstname.value)
+// })
+
+// let lastname=document.getElementById("employee")
+// console.log(lastname.value);
+// document.addEventListener("input",function(){
+//     console.log(lastname.value)
+// })
+
+// let  Managername=document.getElementById(" managername")
+// console.log( Managername.value);
+// document.addEventListener("input",function(){
+//     console.log( Managername.value)
+// })
+
+// let FirstName=document.getElementById(" FirstName")
+// console.log( FirstName.value);
+// document.addEventListener("input",function(){
+//     console.log(FirstName.value)
+// })
+
+// let LastName=document.getElementById("LastName")
+// console.log(LastName.value);
+// document.addEventListener("input",function(){
+//     console.log(LastName.value)
+// })
+
+// let task=document.getElementById("task")
+// console.log(task.value);
+// document.addEventListener("input",function(){
+//     console.log(task.value)
+// })
+
+// let taskid=document.getElementById("taskid")
+// console.log(taskid.value);
+// document.addEventListener("input",function(){
+//     console.log(taskid.value)
+// })
+
+
+let firstname=document.getElementById('firstname');
+firstname.addEventListener("input",()=>{
+    let a=firstname.value
+    console.log(a)
+})
+
+let lastname=document.getElementById('lastname');
+lastname.addEventListener("input",()=>{
+    let b=lastname.value
+    console.log(b)
+})
+
+let FirstName=document.getElementById('ManegerName');
+FirstName.addEventListener("input",()=>{
+    let c=FirstName.value
+    console.log(c)
+})
+
+let LastName=document.getElementById('ManegerName1');
+LastName.addEventListener("input",()=>{
+    let d=LastName.value
+    console.log(d)
+})
+
+let task=document.getElementById('task');
+task.addEventListener("input",()=>{
+    let e=task.value
+    console.log(e)
+})
+
+let taskid=document.getElementById('taskid');
+taskid.addEventListener("input",()=>{
+    let f=taskid.value
+    console.log(f)
+})
+
+let AdditionalNotes=document.getElementById('AdditionalNotes');
+AdditionalNotes.addEventListener("input",()=>{
+    let x=AdditionalNotes.value
+    console.log(x)
+})
+let Instructions=document.getElementById('Instructions')
+Instructions.addEventListener("input",()=>{
+    let y=Instructions.value
+    console.log(y)
+})
+
